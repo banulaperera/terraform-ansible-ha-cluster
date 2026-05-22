e37bb6e5eb3607007bc242a60208d0822405c2e0
Banula Perera <93034784+banulaperera@users.noreply.github.com>
2026-05-22 20:24:58 +0530

    Add initial Terraform configuration for OCI resources and variables

---
+++ terraform/main.tf
@@ -0,0 +1,156 @@
+resource "oci_core_vcn" "main_vcn" {
+  compartment_id = var.compartment_ocid
+  cidr_block     = "10.0.0.0/16"
+  display_name   = "ha-vcn"
+}
+
+resource "oci_core_internet_gateway" "igw" {
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_vcn.main_vcn.id
+  display_name   = "main-igw"
+}
+
+resource "oci_core_route_table" "route_table" {
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_vcn.main_vcn.id
+
+  route_rules {
+    destination       = "0.0.0.0/0"
+    destination_type  = "CIDR_BLOCK"
+    network_entity_id = oci_core_internet_gateway.igw.id
+  }
+}
+
+resource "oci_core_security_list" "security_list" {
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_vcn.main_vcn.id
+
+  ingress_security_rules {
+    protocol = "6"
+    source   = "0.0.0.0/0"
+
+    tcp_options {
+      min = 22
+      max = 22
+    }
+  }
+
+  ingress_security_rules {
+    protocol = "6"
+    source   = "0.0.0.0/0"
+
+    tcp_options {
+      min = 80
+      max = 80
+    }
+  }
+
+  ingress_security_rules {
+    protocol = "6"
+    source   = "0.0.0.0/0"
+
+    tcp_options {
+      min = 8404
+      max = 8404
+    }
+  }
+
+  ingress_security_rules {
+    protocol = "6"
+    source   = "0.0.0.0/0"
+
+    tcp_options {
+      min = 8405
+      max = 8405
+    }
+  }
+
+  egress_security_rules {
+    protocol    = "all"
+    destination = "0.0.0.0/0"
+  }
+}
+
+resource "oci_core_subnet" "public_subnet" {
+  compartment_id      = var.compartment_ocid
+  vcn_id              = oci_core_vcn.main_vcn.id
+  cidr_block          = "10.0.1.0/24"
+  display_name        = "public-subnet"
+  route_table_id      = oci_core_route_table.route_table.id
+  security_list_ids   = [oci_core_security_list.security_list.id]
+  prohibit_public_ip_on_vnic = false
+}
+
+data "oci_core_images" "oracle_linux_images" {
+  compartment_id = var.compartment_ocid
+  operating_system = "Oracle Linux"
+  operating_system_version = "7.9"
+  shape = "VM.Standard.E2.1.Micro"
+}
+
+resource "oci_core_instance" "haproxy" {
+  compartment_id = var.compartment_ocid
+  availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
+  shape = "VM.Standard.E2.1.Micro"
+  display_name = "haproxy-server"
+
+  create_vnic_details {
+    subnet_id = oci_core_subnet.public_subnet.id
+    assign_public_ip = true
+  }
+
+  source_details {
+    source_type = "image"
+    source_id   = data.oci_core_images.oracle_linux_images.images[0]["id"]
+  }
+
+  metadata = {
+    ssh_authorized_keys = var.ssh_public_key
+  }
+}
+
+# resource "oci_core_instance" "nginx" {
+#   compartment_id = var.compartment_ocid
+#   availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
+#   shape = "VM.Standard.E2.1.Micro"
+#   display_name = "nginx-server"
+
+#   create_vnic_details {
+#     subnet_id = oci_core_subnet.public_subnet.id
+#     assign_public_ip = true
+#   }
+
+#   source_details {
+#     source_type = "image"
+#     source_id   = data.oci_core_images.oracle_linux_images.images[0]["id"]
+#   }
+
+#   metadata = {
+#     ssh_authorized_keys = var.ssh_public_key
+#   }
+# }
+
+# resource "oci_core_instance" "apache" {
+#   compartment_id = var.compartment_ocid
+#   availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
+#   shape = "VM.Standard.E2.1.Micro"
+#   display_name = "apache-server"
+
+#   create_vnic_details {
+#     subnet_id = oci_core_subnet.public_subnet.id
+#     assign_public_ip = true
+#   }
+
+#   source_details {
+#     source_type = "image"
+#     source_id   = data.oci_core_images.oracle_linux_images.images[0]["id"]
+#   }
+
+#   metadata = {
+#     ssh_authorized_keys = var.ssh_public_key
+#   }
+# }
+
+data "oci_identity_availability_domains" "ads" {
+  compartment_id = var.tenancy_ocid
+}
---
+++ terraform/outputs.tf
@@ -0,0 +1,11 @@
+# output "haproxy_public_ip" {
+#   value = oci_core_instance.haproxy.public_ip
+# }
+
+# output "nginx_public_ip" {
+#   value = oci_core_instance.nginx.public_ip
+# }
+
+# output "apache_public_ip" {
+#   value = oci_core_instance.apache.public_ip
+# }
