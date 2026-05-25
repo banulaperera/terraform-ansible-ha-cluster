5de089c55489e21bc9b905669f9fa052a73ea311
Banula Perera <93034784+banulaperera@users.noreply.github.com>
2026-05-25 21:43:14 +0530

    Remove HAProxy metrics ingress rule from security group configuration

--- terraform-AWS/main.tf
+++ terraform-AWS/main.tf
@@ -88,14 +88,6 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    description = "HAProxy Metrics"
-    from_port   = 8405
-    to_port     = 8405
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
--- terraform-OCI/main.tf
+++ terraform-OCI/main.tf
@@ -55,16 +55,6 @@ resource "oci_core_security_list" "security_list" {
     }
   }
 
-  ingress_security_rules {
-    protocol = "6"
-    source   = "0.0.0.0/0"
-
-    tcp_options {
-      min = 8405
-      max = 8405
-    }
-  }
-
   egress_security_rules {
     protocol    = "all"
     destination = "0.0.0.0/0"
@@ -72,30 +62,30 @@ resource "oci_core_security_list" "security_list" {
 }
 
 resource "oci_core_subnet" "public_subnet" {
-  compartment_id      = var.compartment_ocid
-  vcn_id              = oci_core_vcn.main_vcn.id
-  cidr_block          = "10.0.1.0/24"
-  display_name        = "public-subnet"
-  route_table_id      = oci_core_route_table.route_table.id
-  security_list_ids   = [oci_core_security_list.security_list.id]
+  compartment_id             = var.compartment_ocid
+  vcn_id                     = oci_core_vcn.main_vcn.id
+  cidr_block                 = "10.0.1.0/24"
+  display_name               = "public-subnet"
+  route_table_id             = oci_core_route_table.route_table.id
+  security_list_ids          = [oci_core_security_list.security_list.id]
   prohibit_public_ip_on_vnic = false
 }
 
 data "oci_core_images" "oracle_linux_images" {
-  compartment_id = var.compartment_ocid
-  operating_system = "Oracle Linux"
+  compartment_id           = var.compartment_ocid
+  operating_system         = "Oracle Linux"
   operating_system_version = "7.9"
-  shape = "VM.Standard.E2.1.Micro"
+  shape                    = "VM.Standard.E2.1.Micro"
 }
 
 resource "oci_core_instance" "haproxy" {
-  compartment_id = var.compartment_ocid
+  compartment_id      = var.compartment_ocid
   availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
-  shape = "VM.Standard.E2.1.Micro"
-  display_name = "haproxy-server"
+  shape               = "VM.Standard.E2.1.Micro"
+  display_name        = "haproxy-server"
 
   create_vnic_details {
-    subnet_id = oci_core_subnet.public_subnet.id
+    subnet_id        = oci_core_subnet.public_subnet.id
     assign_public_ip = true
   }
 
@@ -110,13 +100,13 @@ resource "oci_core_instance" "haproxy" {
 }
 
 resource "oci_core_instance" "nginx" {
-  compartment_id = var.compartment_ocid
+  compartment_id      = var.compartment_ocid
   availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
-  shape = "VM.Standard.E2.1.Micro"
-  display_name = "nginx-server"
+  shape               = "VM.Standard.E2.1.Micro"
+  display_name        = "nginx-server"
 
   create_vnic_details {
-    subnet_id = oci_core_subnet.public_subnet.id
+    subnet_id        = oci_core_subnet.public_subnet.id
     assign_public_ip = true
   }
 
@@ -131,13 +121,13 @@ resource "oci_core_instance" "nginx" {
 }
 
 resource "oci_core_instance" "apache" {
-  compartment_id = var.compartment_ocid
+  compartment_id      = var.compartment_ocid
   availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
-  shape = "VM.Standard.E2.1.Micro"
-  display_name = "apache-server"
+  shape               = "VM.Standard.E2.1.Micro"
+  display_name        = "apache-server"
 
   create_vnic_details {
-    subnet_id = oci_core_subnet.public_subnet.id
+    subnet_id        = oci_core_subnet.public_subnet.id
     assign_public_ip = true
   }
 
